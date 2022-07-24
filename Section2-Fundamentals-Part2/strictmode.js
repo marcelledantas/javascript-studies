dd9418c0c3b9c861b2abb84525f8bd7273fb7b81
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-07-24 18:54:45 -0300

    continue studies

---
+++ Section2-Fundamentals-Part2/strictmode.js
@@ -0,0 +1,10 @@
+'use strict'; //show mistakes
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+//words like interface, private are reserve words in js
+const interface = 'Audio';
