5006b2416da3878d43e0d88260dfd0f8e3efb627
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-10-04 17:08:22 -0300

    refactoring

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,10 +3,10 @@
 // const score1 = document.querySelector('#score--1'); //selecting an ID with #
 // Another way to select an ID with
 const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+let player1El = document.querySelector('.player--1');
+let score0El = document.getElementById('score--0');
+let score1El = document.getElementById('score--1');
+let diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -14,14 +14,25 @@ let currentScore0El = document.getElementById('current--0');
 let currentScore1El = document.getElementById('current--1');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true; //state variable
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true; //state variable
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    currentScore0El.textContent = 0;
+    currentScore1El.textContent = 0;
+    diceEl.classList.add('hidden');
+};
+
+init();
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -90,7 +101,9 @@ btnNew.addEventListener('click', function() {
             .classList.remove('player--winner');
         player0El.classList.add('player--active');
         player1El.classList.remove('player--active');
-        playing = true;
+
+        init();
+        console.log(`score ${scores}`);
     }
 });
 
