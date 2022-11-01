6210c2ae92f537308ecf6afe39266d7850919f1d
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-10-31 22:29:37 -0300

    More of hoisting

--- How-JS-Works/hoisting/script.js
+++ How-JS-Works/hoisting/script.js
@@ -8,8 +8,8 @@
 
 console.log(addDecl(2, 3)); //Call function declaration before it was actually defined
 
-console.log(addExpr(2, 3));
-console.log(addArrow(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
 
 //Functions
 function addDecl(a, b) {
@@ -21,3 +21,32 @@ const addExpr = function(a, b) {
 };
 
 const addArrow = (a, b) => a + b;
+
+// console.log(addExpr2(2, 3));
+console.log(addExpr2); // addExpr2 is undefined at this point, because it is declared as var
+
+var addExpr2 = function(a, b) {
+    return a + b;
+};
+
+if (!numProducts) deleteShoppingcart(); //numProducts at this point is undefined. Undefined is a falsy value!!!
+
+var numProducts = 10;
+
+function deleteShoppingcart() {
+    console.log("All products deleted");
+}
+
+//Best practices when writing code:
+//1. Do not use var. Use const instead, when we do not need to change the variables value. And let when we need to changed;
+//2. Declare the variables at the top of their scope;
+//3. Always declared all of the functions first and use them after the declaration.
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); //checking if x is a propertie of window
+
+console.log(y === window.x);
+console.log(z === window.x);
