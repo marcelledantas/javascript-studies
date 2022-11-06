4c79cb3bc15e355e8c718c1b0cd486ba34ec9bda
Marcelle Dantas <marcellemoraesdantas@gmail.com>
2022-11-05 22:43:49 -0300

    more of this keyword

--- How-JS-Works/thisKeyword/script.js
+++ How-JS-Works/thisKeyword/script.js
@@ -1,42 +1,63 @@
 "use strict";
 console.log(this);
 
-const calcAge = function(birthYear) {
-    console.log(2037 - birthYear);
-    console.log(this); //undefined = regular function call (without a function being attached to an object)
-};
+// const calcAge = function(birthYear) {
+//     console.log(2037 - birthYear);
+//     console.log(this); //undefined = regular function call (without a function being attached to an object)
+// };
 
-calcAge(1991);
+// calcAge(1991);
 
-const calcAgeArrow = (birthYear) => {
-    console.log(2037 - birthYear);
-    console.log(this); //window = arrow function does not have its own this keyword, it inherits from the parent function or parent scope, in this case -> window
-};
+// const calcAgeArrow = (birthYear) => {
+//     console.log(2037 - birthYear);
+//     console.log(this); //window = arrow function does not have its own this keyword, it inherits from the parent function or parent scope, in this case -> window
+// };
 
-//Arrow functions: this = <this of surrounding function (lexical this)>
-// Arrow functions does not have this keyword, it inherits it from a parent function.
-calcAgeArrow(1991);
+// //Arrow functions: this = <this of surrounding function (lexical this)>
+// // Arrow functions does not have this keyword, it inherits it from a parent function.
+// calcAgeArrow(1991);
+
+// const jonas = {
+//     year: 1991,
+//     calcAge: function() {
+//         console.log(2037 - this.year);
+//     },
+// };
+
+// jonas.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge; //method borrowing
+
+// console.log(matilda);
+
+// matilda.calcAge(); // IMPORTANT: this keyword always point to the object that is calling the method, even though the method is writting in jonas object
+
+// const f = jonas.calcAge; //copying a function to a variable, because a function is also a value;
+
+// console.log(f);
+// f(); //this f function is just a regular function, there is no object attached to this function, that is why the error appears in the console.
+
+// var firstName = "Matilda";
 
 const jonas = {
+    firstName: "Jonas",
     year: 1991,
     calcAge: function() {
+        console.log(this);
         console.log(2037 - this.year);
     },
-};
 
-jonas.calcAge();
+    greet: () => {
+        console.log(`Hey ${this.firstName}`);
+    },
+}; //this is not a code block, it is a object literal
 
-const matilda = {
-    year: 2017,
-};
+// jonas.greet(); //undefined, because arrow function does not have its own this keyword
 
-matilda.calcAge = jonas.calcAge; //method borrowing
+// console.log(this.firstName); //undefined -> this is not from the global scope
 
-console.log(matilda);
-
-matilda.calcAge(); // IMPORTANT: this keyword always point to the object that is calling the method, even though the method is writting in jonas object
-
-const f = jonas.calcAge; //copying a function to a variable, because a function is also a value;
-
-console.log(f);
-f(); //this f function is just a regular function, there is no object attached to this function, that is why the error appears in the console.
+// calcAge(1991); //undefined -> global scope (strict mode)
